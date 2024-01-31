363cc726b624a1dfd9182cbafd82f360af5f0ba7
Sanghoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-01-31 12:14:25 +0900

    24-01-31-강상훈: 프로젝트 정리
    # 프로젝트 정리
    
    # 추가 사항
    - bp 적용, view만 분리

---
+++ WebMiniapp/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from models import db
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def create_app():
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] =\
+      'sqlite:///' + os.path.join(basedir, 'database.db')
+
+  db.init_app(app)
+  db.app = app
+
+  with app.app_context():
+    db.create_all()
+
+  from views import main_view, music_view
+  
+  app.register_blueprint(main_view.bp)
+  app.register_blueprint(music_view.bp)
+  return app
+
