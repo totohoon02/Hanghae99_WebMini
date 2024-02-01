595c8b6a768bf8c1424f20e934b293a9de7f4a23
qkrckstjq <cksk49@naver.com>
2024-02-01 12:48:34 +0900

    feat 친구 추가 기능

--- views/home_view.py
+++ views/home_view.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, Blueprint
 import os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from models import db, notice_board_list, User
+from models import db, notice_board_list, User, Friend
 
 bp = Blueprint('home', __name__, url_prefix="/")
 
 @bp.route("/",methods=["GET"])
 def home():
     input_keyword = request.args.get('query')
+    user_cookie_id = request.cookies.get('user_id')
 
     if input_keyword:
         notice_list = notice_board_list.query.filter(
@@ -17,11 +18,13 @@ def home():
         ).all()
     else:
         notice_list = notice_board_list.query.all()
-
-    print("아래와 같음")
-    print(notice_list)
     
-    return render_template('home.html', data=notice_list)
+    if user_cookie_id:
+        friend_list = Friend.query.filter_by(user_id=user_cookie_id).all()   
+    
+    print(friend_list)
+    
+    return render_template('home.html', data=notice_list, friendList=friend_list)
 
 @bp.route("/board", methods=['POST'])
 def submitwish():
@@ -38,5 +41,24 @@ def submitwish():
         print("사용자 정보를 찾을 수 없습니다.")
     return render_template('home.html')
 
+@bp.route("/friend", methods=['POST'])
+def addfriend():
+    jsonData = request.get_json()
+    friend_id_req = jsonData.get("user_id")
+    user_cookie_id = request.cookies.get('user_id')
+    user_name = User.query.filter_by(user_id=user_cookie_id).first().username
+    exist_friend = Friend.query.filter_by(user_id=user_cookie_id, friend_id = friend_id_req).first()
+    
+    exist_friend = Friend.query.filter_by(user_id=user_cookie_id, friend_id=friend_id_req).first()
+
+    if user_cookie_id and not exist_friend:
+        new_friend_row = Friend(user_id=user_cookie_id, username=user_name, friend_id=friend_id_req)
+        print(new_friend_row)
+        db.session.add(new_friend_row)
+        db.session.commit()
+
+    friend_list = Friend.query.filter_by(user_id=user_cookie_id).all()
+
+    return render_template('home.html', friendList=friend_list)
     
     
