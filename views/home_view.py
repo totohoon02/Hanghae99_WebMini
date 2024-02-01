51a3373a2f73566dd72562a500e0b657197671eb
soijeongg <010127js@gmail.com>
2024-02-01 11:56:17 +0900

    카드누르면 상세페이지

--- views/home_view.py
+++ views/home_view.py
@@ -6,7 +6,8 @@ from models import db, notice_board_list, User
 
 bp = Blueprint('home', __name__, url_prefix="/")
 
-@bp.route("/",methods=["GET"])
+
+@bp.route("/", methods=["GET"])
 def home():
     input_keyword = request.args.get('query')
 
@@ -20,23 +21,38 @@ def home():
 
     print("아래와 같음")
     print(notice_list)
-    
-    return render_template('home.html', data=notice_list)
+
+    if 'user_id' in request.cookies:
+        # 쿠키가 있을 때 다른 페이지로 랜더링
+        return render_template('home2.html', data=notice_list)
+    else:
+        # 쿠키가 없을 때 현재 페이지 랜더링
+        return render_template('home.html', data=notice_list)
+
 
 @bp.route("/board", methods=['POST'])
 def submitwish():
     jsonData = request.get_json()
     userId = jsonData.get('user_id')
     contents = jsonData.get('contents')
-    
-    user = User.query.filter_by(user_id=1).first()
+
+    user = User.query.filter_by(user_id=userId).first()
     if user:
-        newBoard = notice_board_list(user_id=1, username='박찬섭', contents=contents)
+        newBoard = notice_board_list(
+            user_id=userId, username=user.username, contents=contents)
         db.session.add(newBoard)
         db.session.commit()
     else:
         print("사용자 정보를 찾을 수 없습니다.")
+
     return render_template('home.html')
 
-    
-    
+
+@bp.route("/<wishid>")
+def godetail(wishid):
+  # 글 내용, 댓글리스트 가져오기
+  board = notice_board_list.query.filter(
+      notice_board_list.board_id == wishid).one()
+  return render_template("myPageDetail.html", data={
+      "board": board,
+  })
